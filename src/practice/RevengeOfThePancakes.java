baa8704d026d3d2a36cd50248aa59d93708a58dc
JMYang <jaewonmichael@gmail.com>
2017-04-07 10:45:43 -0700

    ROTP

---
+++ src/practice/RevengeOfThePancakes.java
@@ -0,0 +1,69 @@
+package practice;
+
+import utilities.ChallengeBase;
+
+public class RevengeOfThePancakes extends ChallengeBase
+{
+    @Override
+    public String Manipulate(String input)
+    {
+        Integer numFlips = 0;
+        Boolean isCorrect = false;
+        char[] chars = input.toCharArray();
+
+        while (!isCorrect)
+        {
+            char lastChar = chars[0];
+            Integer finalIndex = -1;
+
+            for(int i = 1; i < chars.length; i++)
+            {
+                if(chars[i] != lastChar)
+                {
+                    finalIndex = i;
+                    break;
+                }
+                else
+                {
+                    lastChar = chars[i];
+                }
+            }
+
+            if(finalIndex == -1)
+            {
+                if(chars[0] == '-')
+                {
+                    finalIndex = chars.length - 1;
+                    flipChars(finalIndex, chars);
+                    numFlips++;
+                }
+
+                isCorrect = true;
+            }
+            else
+            {
+                flipChars(finalIndex - 1, chars);
+                numFlips++;
+            }
+        }
+
+        return numFlips.toString();
+    }
+
+    private char[] flipChars(int max, char[] input)
+    {
+        char[] chars = input;
+        for(int i = 0; i < max + 1; i++)
+        {
+            if(chars[i] == '-')
+            {
+                chars[i] = '+';
+            }
+            else
+            {
+                chars[i] = '-';
+            }
+        }
+        return chars;
+    }
+}
